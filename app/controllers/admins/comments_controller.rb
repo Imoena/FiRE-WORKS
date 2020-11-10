9852717786b50ac5c08fb168a05c2fecfd7870e1
vagrant <vagrant@localhost.localdomain>
2020-11-10 12:12:11 +0000

    [Fix]Admin_comment

--- app/controllers/admins/comments_controller.rb
+++ app/controllers/admins/comments_controller.rb
@@ -1,35 +1,35 @@
-class　Admins::CommentsController < ApplicationController
-  before_action :authenticate_admin!
+class Admins::CommentsController < ApplicationController
+#   before_action :authenticate_admin!
 
-  def create
-   @post = Post.find(params[:post_id])
-   @comment = @post.comments.new(comment_params)
-   @comment_reply = @post.comments.new
-   @comment.user_id = current_user.id
-   if @comment.save
-    @comment = Comment.new
-   else
-    @comment = Comment.new
-      redirect_back(fallback_location: root_path)
-    end
-  end
+#   def create
+#    @post = Post.find(params[:post_id])
+#    @comment = @post.comments.new(comment_params)
+#    @comment_reply = @post.comments.new
+#    @comment.user_id = current_user.id
+#    if @comment.save
+#     @comment = Comment.new
+#    else
+#     @comment = Comment.new
+#       redirect_back(fallback_location: root_path)
+#     end
+#   end
 
-  def destroy
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
-    @post = @comment.post
-   if @comment.user != current_user
-    # リダイレクト先考える
-      redirect_to request.referer
-   else
-      @comment.destroy
-      @new_comment = Comment.new
-   end
-  end
+#   def destroy
+#     @post = Post.find(params[:post_id])
+#     @comment = Comment.find(params[:id])
+#     @post = @comment.post
+#    if @comment.user != current_user
+#     # リダイレクト先考える
+#       redirect_to request.referer
+#    else
+#       @comment.destroy
+#       @new_comment = Comment.new
+#    end
+#   end
 
-  private
+#   private
 
-  def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, :parent_id)
-  end
+#   def comment_params
+#     params.require(:comment).permit(:content, :post_id, :user_id, :parent_id)
+#   end
 end
