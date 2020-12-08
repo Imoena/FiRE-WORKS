66f5b24c4191d9381fb68236c9ac18fd041b01b4
Imoena <>
2020-12-08 05:28:06 +0000

    test

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -3,8 +3,11 @@ class Users::HomesController < ApplicationController
     @posts = Post.limit(3).sort { |a, b| b.liking_users.count <=> a.liking_users.count } # 投稿のいいね数を比較して降順で表示
     @events = Event.all
     @user = current_user
-    @topic_event = Event.where(prefecture: @user.prefecture)#ユーザーが登録した都道府県のeventを配列で探す
-    @user_prefecture_posts = Post.where(prefecture: @user.prefecture)
+    if user_signed_in?
+      @topic_event = Event.where(prefecture: @user.prefecture)#ユーザーが登録した都道府県のeventを配列で探す
+      @user_prefecture_posts = Post.where(prefecture: @user.prefecture)
+    else
+    end
   end
 
   def about
